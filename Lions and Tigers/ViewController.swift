743554d1fcbe29034cab8c7cc634008e9c4ddde0
riddell999 <dell.smith999@gmail.com>
2014-12-04 16:20:38 -0600

    Initial Commit

---
+++ Lions and Tigers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lions and Tigers
+//
+//  Created by Dell Smith on 12/4/14.
+//  Copyright (c) 2014 Dell Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
